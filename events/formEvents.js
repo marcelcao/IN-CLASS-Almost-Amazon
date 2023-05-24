6ab2623fa2cb7a7f1bc1f3c0dd83dfa6f326f53e
Mara Caoile <mara.caoile@gmail.com>
2023-05-23 21:01:22 -0500

    created functions to add books and updated books

--- events/formEvents.js
+++ events/formEvents.js
@@ -1,14 +1,45 @@
+import { createBook, updateBook, getBooks } from '../api/bookData';
+import { showBooks } from '../pages/books';
+
 const formEvents = () => {
   document.querySelector('#main-container').addEventListener('submit', (e) => {
     e.preventDefault();
-    // TODO: CLICK EVENT FOR SUBMITTING FORM FOR ADDING A BOOK
+    // CLICK EVENT FOR SUBMITTING FORM FOR ADDING A BOOK
     if (e.target.id.includes('submit-book')) {
+      const payload = {
+        title: document.querySelector('#title').value,
+        description: document.querySelector('#description').value,
+        image: document.querySelector('#image').value,
+        price: document.querySelector('#price').value,
+        author_id: document.querySelector('#author_id').value,
+        sale: document.querySelector('#sale').checked,
+      };
+
+      createBook(payload).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+
+        updateBook(patchPayload).then(() => {
+          getBooks().then(showBooks);
+        });
+      });
       console.warn('CLICKED SUBMIT BOOK', e.target.id);
     }
 
-    // TODO: CLICK EVENT FOR EDITING A BOOK
+    // CLICK EVENT FOR EDITING A BOOK
     if (e.target.id.includes('update-book')) {
       const [, firebaseKey] = e.target.id.split('--');
+      const payload = {
+        title: document.querySelector('#title').value,
+        description: document.querySelector('#description').value,
+        image: document.querySelector('#image').value,
+        price: document.querySelector('#price').value,
+        author_id: document.querySelector('#author_id').value,
+        sale: document.querySelector('#sale').checked,
+        firebaseKey,
+      };
+      updateBook(payload).then(() => {
+        getBooks().then(showBooks);
+      });
       console.warn('CLICKED UPDATE BOOK', e.target.id);
       console.warn(firebaseKey);
     }
