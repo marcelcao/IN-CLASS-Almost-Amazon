90207e7e660e20d1d6d7a4e8b0962040963021c6
Mara Caoile <mara.caoile@gmail.com>
2023-05-26 09:57:06 -0500

    still has viewAuthor errors - books are undefined

--- pages/viewAuthor.js
+++ pages/viewAuthor.js
@@ -8,21 +8,23 @@ const viewAuthor = (obj) => {
      ${obj.first_name} ${obj.last_name} ${obj.favorite ? '<span class="badge bg-danger"><i class="fa fa-heart" aria-hidden="true"></i></span>' : ''}</h5>
      Author Email: <a href="mailto:${obj.email}">${obj.email}</a>     
   </div>`;
-  let bookString = '';
-  obj.forEach((item) => {
-    bookString += `<div class="card">
-    <img class="card-img-top" src=${item.image} alt=${item.title} style="height: 400px;">
-    <div class="card-body" style="height: 180px;">
-      <h5 class="card-title">${item.title}</h5>
-        <p class="card-text bold">${item.sale ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${item.price}` : `$${item.price}`}</p>
-        <hr>
-        <i class="btn btn-success fas fa-eye" id="view-book-btn--${item.firebaseKey}"></i>
-        <i id="edit-book-btn--${item.firebaseKey}" class="fas fa-edit btn btn-info"></i>
-        <i id="delete-book-btn--${item.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
-    </div>
-  </div>`;
+  obj.booksArray.forEach((book) => {
+    domString += `<div class="text-white ms-5 details">
+      <h5 class="text-white">${book.title}  ${book.favorite ? '<span class="badge bg-danger"><i class="fa fa-heart" aria-hidden="true"></i></span>' : ''}</h5>
+      <p>${book.description || ''}</p>
+      <hr>
+      <p>${book.sale ? `<span class="badge bg-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span>
+        $${book.price}` : `$${book.price}`}</p>
+      </div>
+      <div class="d-flex flex-column">
+        <img src=${book.image} alt=${book.title} style="width: 100px;">
+        <div class="mt-5">
+          <i id="edit-book-btn--${book.firebaseKey}" class="fas fa-edit btn btn-info"></i>
+          <i id="delete-book--${book.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
+        </div>
+      </div>
+      </div>`;
   });
-  domString += domString + bookString;
   renderToDOM('#view', domString);
 };
 
