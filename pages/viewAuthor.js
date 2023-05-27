769021e00cc015a2e23cc298bd78476ee03b4769
Mara Caoile <mara.caoile@gmail.com>
2023-05-26 23:01:09 -0400

    fixed author books in author details

--- pages/viewAuthor.js
+++ pages/viewAuthor.js
@@ -9,21 +9,18 @@ const viewAuthor = (obj) => {
      Author Email: <a href="mailto:${obj.email}">${obj.email}</a>     
   </div>`;
   obj.booksArray.forEach((book) => {
-    domString += `<div class="text-white ms-5 details">
-      <h5 class="text-white">${book.title}  ${book.favorite ? '<span class="badge bg-danger"><i class="fa fa-heart" aria-hidden="true"></i></span>' : ''}</h5>
-      <p>${book.description || ''}</p>
-      <hr>
-      <p>${book.sale ? `<span class="badge bg-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span>
-        $${book.price}` : `$${book.price}`}</p>
-      </div>
-      <div class="d-flex flex-column">
-        <img src=${book.image} alt=${book.title} style="width: 100px;">
-        <div class="mt-5">
+    domString += `
+    <div class="card-authorbooks">
+      <img class="card-img-top" src=${book.image} alt=${book.title} style="height: 400px;">
+      <div class="card-body" style="height: 180px;">
+        <h5 class="card-title">${book.title}</h5>
+          <p class="card-text bold">${book.sale ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${book.price}` : `$${book.price}`}</p>
+          <hr>
+          <i class="btn btn-success fas fa-eye" id="view-book-btn--${book.firebaseKey}"></i>
           <i id="edit-book-btn--${book.firebaseKey}" class="fas fa-edit btn btn-info"></i>
-          <i id="delete-book--${book.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
-        </div>
+          <i id="delete-book-btn--${book.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
       </div>
-      </div>`;
+    </div>`;
   });
   renderToDOM('#view', domString);
 };
